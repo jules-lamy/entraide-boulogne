80d932e03c2d1148830857d37e514a35106c0ad9
Jules Lamy <73222407+jules-lamy@users.noreply.github.com>
2023-06-12 15:06:00 +0200

    Various updates for summer
    - Add summer break closure
    - "Grande Braderie" add

--- script.js
+++ script.js
@@ -10,31 +10,35 @@ function updateStatus() {
   var statusText = "L'Entraide est actuellement fermée pour tous vos dépôts";
 
   // Périodes exceptionnelles
-  var exceptionOpenStartDate = new Date('2029-05-24 00:00:00');
-  var exceptionOpenEndDate = new Date('2029-05-25 00:00:00');
-  var exceptionClosedStartDate = new Date('2029-12-25 00:00:00');
-  var exceptionClosedEndDate = new Date('2029-12-31 00:00:00');
+  var exceptionOpenStartDate = new Date('2029-06-12 00:00:00');
+  var exceptionOpenEndDate = new Date('2029-09-04 00:00:00');
+  var exceptionClosedStartDate = new Date('2023-06-12 00:00:00');
+  var exceptionClosedEndDate = new Date('2023-09-04 00:00:00');
 
-  // Vérifier si l'Entraide est ouverte
-  if (
+  // Vérifier si l'Entraide est ouverte pour une période exceptionnelle
+  if (currentDate >= exceptionOpenStartDate && currentDate <= exceptionOpenEndDate) {
+    isOpen = true;
+    backgroundColor = '#46b154';
+    statusText = "L'Entraide est actuellement ouverte pour tous vos dépôts (période exceptionnelle ouverte)";
+  }
+
+  // Vérifier si l'Entraide est fermée pour une période exceptionnelle
+  else if (currentDate >= exceptionClosedStartDate && currentDate <= exceptionClosedEndDate) {
+    isOpen = false;
+    backgroundColor = '#e60000';
+    statusText = "L'Entraide est fermée pour tous vos dépôts jusqu'au 4 septembre";
+  }
+
+  // Vérifier si l'Entraide est ouverte pour la période habituelle
+  else if (
     // Période régulière (lundi de 14h à 17h, mardi de 9h à 12h, samedi de 9h à 12h)
     (currentDay === 1 && currentHour >= 14 && currentHour < 17) || 
     (currentDay === 2 && currentHour >= 9 && currentHour < 12) || 
-    (currentDay === 6 && currentHour >= 9 && currentHour < 12) ||
-
-    // Période exceptionnelle "ouverte"
-    (currentDate >= exceptionOpenStartDate && currentDate <= exceptionOpenEndDate)
+    (currentDay === 6 && currentHour >= 9 && currentHour < 12)
   ) {
     isOpen = true;
     backgroundColor = '#46b154';
     statusText = "L'Entraide est actuellement ouverte pour tous vos dépôts";
-  }
-
-  // Vérifier si l'Entraide est fermée (période exceptionnelle "fermée")
-  else if (currentDate >= exceptionClosedStartDate && currentDate <= exceptionClosedEndDate) {
-    isOpen = false;
-    backgroundColor = '#e60000';
-    statusText = "L'Entraide est actuellement fermée pour tous vos dépôts";
   }
 
   // Mettre à jour l'arrière-plan et le texte en conséquence
