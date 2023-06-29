d06e8ad36027c4d22523818abdf8bcef00bb4a2b
Jules Lamy <73222407+jules-lamy@users.noreply.github.com>
2023-06-29 13:56:05 +0200

    Add Pop up for Holidays

--- script.js
+++ script.js
@@ -52,3 +52,28 @@ window.addEventListener('DOMContentLoaded', updateStatus);
 
 // Actualiser périodiquement l'état
 setInterval(updateStatus, 60000); // Actualiser toutes les 60 secondes
+
+
+
+
+
+
+// Obtenez la référence à la fenêtre modale et au bouton de fermeture
+var modal = document.getElementById("myModal");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Affichez la fenêtre modale lorsque la page se charge pour la première fois
+window.onload = function () {
+  modal.style.display = "block";
+};
+
+// Fermez la fenêtre modale lorsque l'utilisateur clique sur le bouton de fermeture ou en dehors de la fenêtre
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
